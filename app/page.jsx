948e8c10266593b16147635ff8a516f33f7fc7dc
RItu Raj <rajritu2084@gmail.com>
2025-03-17 22:00:35 +0530

    initial project setup and routing

---
+++ app/page.jsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <div className="text-3xl text-red-600">Home Page</div>;
+};
+
+export default HomePage;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export const content = [
+  './pages/**/*.{js,ts,jsx,tsx,mdx}',
+  './components/**/*.{js,ts,jsx,tsx,mdx}',
+  './pages/**/*.{js,ts,jsx,tsx,mdx}',
+];
+export const theme = {
+  extend: {
+    fontFamily: {
+      sans: ['Poppins', 'sans-sarif'],
+    },
+    gridTemplateColumns: {
+      '70/30': '70% 28%',
+    },
+  },
+};
+export const plugins = [];
