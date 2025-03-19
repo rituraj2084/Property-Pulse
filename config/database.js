88019f872ada1741de16a75312f06cb66abcba10
RItu Raj <rajritu2084@gmail.com>
2025-03-19 18:15:49 +0530

    Adding PropertyDetails page

--- config/database.js
+++ config/database.js
@@ -10,7 +10,6 @@ const connectDB = async () => {
   }
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log('Database connected successfully!');
     connected = true;
   } catch (error) {
     console.error(error);
