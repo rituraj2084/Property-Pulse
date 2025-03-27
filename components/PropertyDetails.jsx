f9bc03d142e6606778350672790975862de3d56b
RItu Raj <rajritu2084@gmail.com>
2025-03-28 00:56:30 +0530

    Adding Maps

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -7,6 +7,7 @@ import {
   FaRulerCombined,
   FaTimes,
 } from 'react-icons/fa';
+import PropertyMap from './PropertyMap';
 const PropertyDetails = ({ property }) => {
   return (
     <main>
@@ -91,7 +92,7 @@ const PropertyDetails = ({ property }) => {
       </div>
       {/* <!-- Map --> */}
       <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-        <div id="map"></div>
+        <PropertyMap property={property} />
       </div>
     </main>
   );
