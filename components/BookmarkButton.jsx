a9004c5fb3c9490f94d491290f4823e2393ba2fe
RItu Raj <rajritu2084@gmail.com>
2025-03-29 19:42:38 +0530

    Adding bookmarks functionality

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -1,22 +1,52 @@
 'use client';
 import bookmarkProperty from '@/app/actions/bookmarkProperty';
+import checkBookmarkStatus from '@/app/actions/checkBookmarkStatus';
 import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
 import { FaBookmark } from 'react-icons/fa';
 import { toast } from 'react-toastify';
 
 const BookmarkButton = ({ property }) => {
   const { data: session } = useSession();
   const userId = session?.user?.id;
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!userId) {
+      setLoading(false);
+      return;
+    }
+    checkBookmarkStatus(property._id).then((res) => {
+      if (res.error) toast.error(res.error);
+      if (res.isBookmarked) setIsBookmarked(res.isBookmarked);
+      setLoading(false);
+    });
+  }, [property._id, userId, checkBookmarkStatus]);
+
   const handleBookmarkClick = async () => {
     if (!userId) {
       toast.error('You need to be signed in to bookmark a listing!');
     }
     bookmarkProperty(property._id).then((res) => {
       if (res.error) return toast.error(res.error);
+      setIsBookmarked(res.isBookmarked);
       toast.success(res.message);
     });
   };
-  return (
+
+  if (loading) {
+    return <p className="text-center text-2xl">Loading...</p>;
+  }
+
+  return isBookmarked ? (
+    <button
+      className="bg-red-500 cursor-pointer hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+      onClick={handleBookmarkClick}
+    >
+      <FaBookmark className="mr-2" /> Remove Property
+    </button>
+  ) : (
     <button
       className="bg-blue-500 cursor-pointer hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
       onClick={handleBookmarkClick}
